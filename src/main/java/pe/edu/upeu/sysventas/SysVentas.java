24c08c988ab2cf7c99f862353c1c09d356a35ea3
AndersonDiazDev <diazccamathomas@gmail.com>
2026-09-14 10:04:51 -0500

    Avance del proyecto

--- src/main/java/pe/edu/upeu/sysventas/SysVentas.java
+++ src/main/java/pe/edu/upeu/sysventas/SysVentas.java
@@ -2,19 +2,33 @@ package pe.edu.upeu.sysventas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import pe.edu.upeu.sysventas.config.AppContext;
 
 import java.io.IOException;
 
 public class SysVentas extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
-        stage.setTitle("Hello!");
+        AppContext context=AppContext.getInstance();
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                SysVentas.class.getResource("/view/main_producto.fxml")
+        );
+        fxmlLoader.setControllerFactory(context::getBean);
+        Screen screen=Screen.getPrimary();
+        Rectangle2D r2d=screen.getVisualBounds();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Sistema de Ventas");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
